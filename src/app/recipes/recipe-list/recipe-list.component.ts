87e0c44117da7c18cf727cfaa0fe5da5de769ebd
Kirankumar Sripati <krnsripati@gmail.com>
2020-08-26 09:48:56 +0530

    add recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,9 +9,14 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     {
-      name: 'A Test Recipe',
-      description: 'This is simple a test',
-      imagePath: 'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1335&q=80',
+      name: 'Vada Pav',
+      description: 'Maharashtra Special',
+      imagePath: 'https://media.istockphoto.com/photos/vada-pav-with-a-glass-of-tea-very-famous-street-food-of-india-in-the-picture-id1248186164',
+    },
+    {
+      name: 'Misal Pav',
+      description: 'Mamledar Special',
+      imagePath: 'https://hebbarskitchen.com/wp-content/uploads/mainPhotos/misal-pav-recipe-how-to-make-maharashtrian-misal-pav-recipe-1-1068x1423.jpeg',
     }
   ];
 
