5150e107deceaf38ad17ba2e164a5b5b1a8fa0e5
Kirankumar Sripati <krnsripati@gmail.com>
2020-08-30 18:33:45 +0530

    add basic form  in shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<{name: string, amount: number}>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddItem(): void {
+    this.ingredientAdded.emit({
+      name: this.nameInputRef.nativeElement.value + '',
+      amount: +this.amountInputRef.nativeElement.value,
+    });
+  }
 }
