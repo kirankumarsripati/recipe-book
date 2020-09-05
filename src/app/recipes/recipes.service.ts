73f9b750b9f17cb0b677765d38a32504046096b4
Kirankumar Sripati <krnsripati@gmail.com>
2020-09-06 00:57:46 +0530

    Adding Ingredients to Recipes

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -9,11 +9,31 @@ export class RecipeService {
       name: 'Vada Pav',
       description: 'Maharashtra Special',
       imagePath: '//media.istockphoto.com/photos/vada-pav-with-a-glass-of-tea-very-famous-street-food-of-india-in-the-picture-id1248186164',
+      ingredients: [
+        {
+          name: 'Potato',
+          amount: 5,
+        },
+        {
+          name: 'Pav',
+          amount: 10,
+        },
+      ],
     },
     {
       name: 'Misal Pav',
       description: 'Mamledar Special',
       imagePath: '//media.istockphoto.com/photos/misal-pavindian-street-food-picture-id1222977707',
+      ingredients: [
+        {
+          name: 'Misal',
+          amount: 5,
+        },
+        {
+          name: 'Pav',
+          amount: 10,
+        },
+      ],
     }
   ];
 
