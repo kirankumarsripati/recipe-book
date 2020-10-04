c2a46223bb3d7a5236fb7fe8a21f53ca348819d8
Kirankumar Sripati <krnsripati@gmail.com>
2020-10-04 16:05:34 +0530

    Attaching the Token with an Interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -5,7 +5,6 @@ import { exhaustMap, map, take, tap } from 'rxjs/operators';
 import { RecipeService } from '../recipes/recipes.service';
 import { Recipe } from '../recipes/recipe.model';
 import { Observable } from 'rxjs';
-import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +13,6 @@ export class DataStorageService {
   constructor(
     private http: HttpClient,
     private recipeService: RecipeService,
-    private authService: AuthService,
   ) {}
 
   storeRecipes(): void {
@@ -26,28 +24,21 @@ export class DataStorageService {
   }
 
   fetchRecipes(): Observable<Recipe[]> {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http
-        .get<Recipe[]>(
-          'https://ng-course-recipe-book2-kk.firebaseio.com/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token),
-          }
-        );
-      }),
-      map(recipes => {
-        return recipes.map(recipe => {
-          return {
-            ...recipe,
-            ingredients: recipe.ingredients ? recipe.ingredients : [],
-          }
-        });
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-      })
-    );
+    return this.http
+      .get<Recipe[]>(
+        'https://ng-course-recipe-book2-kk.firebaseio.com/recipes.json',
+      ).pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            }
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
