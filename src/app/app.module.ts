e0934386e3618c23af20af589a8c7fed1cad3e2b
Kirankumar Sripati <krnsripati@gmail.com>
2020-10-11 09:05:03 +0530

    add auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,6 @@ import { AuthModule } from './auth/auth.module';
     HttpClientModule,
     AppRoutingModule,
     CoreModule,
-    AuthModule,
   ],
   bootstrap: [AppComponent]
 })
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { AuthComponent } from './auth.component';
     FormsModule,
     SharedModule,
     RouterModule.forChild([
-      { path: 'auth', component: AuthComponent },
+      { path: '', component: AuthComponent },
     ])
   ]
 })
